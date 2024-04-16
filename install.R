87c57a1d77d96f4ad3d36bf0aa55a300415cfed6
Egor Kotov <e-kotov@users.noreply.github.com>
2024-04-16 15:45:20 +0300

    fix typo

--- install.R
+++ install.R
@@ -1,7 +1,7 @@
 # just add lines with install.packages() to add packages
 # that are not included in the base container image (see Dockerfile file in the repository).
 
-# We are only installing one pacakge from GitHub, so we use
+# We are only installing one package from GitHub, so we use
 # remotes package (already preinstalled in rocker/binder that we use as the fodunation/base).
 # remotes::install_github() function installs an R package from github.com and the provided
 # user and repository name in the format <github_user>/<repository_name>.
